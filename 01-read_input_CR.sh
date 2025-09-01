e0c8b56acccfb92b6ed5a969633550f031bafa70
XavierJames <djwildstyle3@gmail.com>
2025-08-31 20:51:53 -0400

    added copy of input file

---
+++ 01-read_input_CR.sh
@@ -0,0 +1,9 @@
+echo "Hello! My name is xavier james"
+# read name
+# echo "Welcome, $name"
+
+# single quotes prevent the expansion of the variable
+# echo 'Your name was stored in $name'
+
+# exercise: write a script that asks the user for a 
+# filename and create an empty file named after it
