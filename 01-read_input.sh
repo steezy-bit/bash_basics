662bb4601659dce94302261044b57fee653600fe
XavierJames <djwildstyle3@gmail.com>
2025-09-07 18:30:48 -0400

    Alternative solution for exercise in 01-read_input.sh

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -7,6 +7,18 @@ echo "Welcome, $name"
 
 # single quotes prevent the expansion of the variable
 echo 'Your name was stored in $name'
+ 
+# exercise: ask the user for a filename
+echo "Please enter a filename:"
+read filename
 
-# exercise: write a script that asks the user for a 
-# filename and create an empty file named after it
+# create an empty file with that name
+touch "$filename"
+echo "Created an empty file called $filename"
+
+# optional: check that the file really exists
+if [ -e "$filename" ]; then
+  echo "Confirmed: $filename was created successfully."
+else
+  echo "Oops! Something went wrong."
+fi
